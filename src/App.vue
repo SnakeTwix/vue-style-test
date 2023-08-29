5502ca9d3f828691d9276d510885a8dcbd71b9d8
Kuvshinov Alexey <poqe64@gmail.com>
2023-08-30 02:04:10 +0300

    Stuff

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>I'm a parent block!!!</div>
+
+  <div class="box a">AAA</div>
+  <div class="box b">BBB</div>
+  <div class="box c">CCC</div>
+
+  <ChildBlock />
+</template>
+
+<script setup lang="ts">
+import ChildBlock from './ChildBlock.vue';
+</script>
+
+// Block Scoped
+<style scoped>
+.b {
+  background: lime;
+}
+
+.c {
+  background: cyan;
+}
+</style>
+
+// Global style
+<style>
+.box {
+  height: 100px;
+  width: 100px;
+  border: 5px dotted black;
+}
+
+.a {
+  background: red;
+}
+</style>
---
+++ src/ChildBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="child">
+    <div>I'm a child block!!!</div>
+    <div class="box a">child AAA</div>
+    <div class="box b">child BBB</div>
+    <div class="box c">child CCC</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.child {
+  border: 1px solid black;
+}
+
+.b {
+  background: yellow;
+}
+</style>
